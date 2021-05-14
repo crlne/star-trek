66d6e5d436fc6fdd260b1f454a6ded8848472c5b
crlne <carol.lages@outlook.com>
2021-05-14 20:04:50 -0300

    title/paragraph page and styles

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+   <>
+    <h1 className={styles.title}>USS DISCOVERY</h1>
+
+    <p className={styles.subtitle}>
+    "This ship bears the name Discovery. Never has that been more fitting,<br />
+     or more prescient. She has carried us into the future,<br /> 
+     and it will be our privilege to make that future bright. Let us begin. Together."<br />
+      – Captain Saru, 3189 ("People of Earth")
+    </p>
+   </>
+  )
+}
